048e242de0c195ad67e3e01703528050462ebde7
Mcebisi Mkhohliwe <mcebisi.mkhohliwe@sourcing-sa.co.za>
2020-08-02 19:17:43 +0200

    Added student input type

---
+++ Registration.API/GraphQL/Types/MutationTypes/StudentInputType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+
+namespace Registration.API.GraphQL.Types.MutationTypes
+{
+    public class StudentInputType:InputObjectGraphType
+    {
+        public StudentInputType()
+        {
+            Field<NonNullGraphType<StringGraphType>>("name");
+            Field<NonNullGraphType<StringGraphType>>("surname");
+            Field<NonNullGraphType<IntGraphType>>("addressId");
+            Field<NonNullGraphType<StringGraphType>>("cellphone");
+            Field<NonNullGraphType<StringGraphType>>("idNumber");
+            Field<NonNullGraphType<IntGraphType>>("courseId");
+        }
+    }
+}
