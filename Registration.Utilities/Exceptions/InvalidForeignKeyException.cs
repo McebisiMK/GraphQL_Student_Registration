43e427893a0ee76cea2cb83383afc0215f2e94ec
Mcebisi Mkhohliwe <mcebisi.mkhohliwe@sourcing-sa.co.za>
2020-08-02 19:25:41 +0200

    Added custom exceptions

---
+++ Registration.Utilities/Exceptions/InvalidForeignKeyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registration.Utilities.Exceptions
+{
+    public class InvalidForeignKeyException : Exception
+    {
+        private static string Message = "Given foreign key(s) does not exists:";
+        public InvalidForeignKeyException(string entity) : base($"{Message} {entity}")
+        {
+        }
+    }
+}
---
+++ Registration.Utilities/Exceptions/InvalidUserObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registration.Utilities.Exceptions
+{
+    public class InvalidUserObject : Exception
+    {
+        private readonly static string Message = "Invalid given object:";
+        public InvalidUserObject(string objectName) : base($"{Message} {objectName}")
+        {
+
+        }
+    }
+}
