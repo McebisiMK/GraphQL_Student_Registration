f03e73224d8a83b9d8b87ddbea7e44869cceb1df
Mcebisi Mkhohliwe <mcebisi.mkhohliwe@sourcing-sa.co.za>
2020-08-02 19:17:26 +0200

    Added subject input type

---
+++ Registration.API/GraphQL/Types/MutationTypes/SubjectInputType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace Registration.API.GraphQL.Types.MutationTypes
+{
+    public class SubjectInputType:InputObjectGraphType
+    {
+        public SubjectInputType()
+        {
+            Field<NonNullGraphType<StringGraphType>>("name");
+            Field<NonNullGraphType<IntGraphType>>("courseId");
+            Field<NonNullGraphType<IntGraphType>>("semesterId");
+        }
+    }
+}
