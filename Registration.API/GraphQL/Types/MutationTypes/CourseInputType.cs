c4382812a9c21475dbbe49ae2cce4e0897f03f87
Mcebisi Mkhohliwe <mcebisi.mkhohliwe@sourcing-sa.co.za>
2020-08-02 19:16:48 +0200

    Added course input type

---
+++ Registration.API/GraphQL/Types/MutationTypes/CourseInputType.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+
+namespace Registration.API.GraphQL.Types.MutationTypes
+{
+    public class CourseInputType : InputObjectGraphType
+    {
+        public CourseInputType()
+        {
+            Field<NonNullGraphType<StringGraphType>>("name");
+        }
+    }
+}
