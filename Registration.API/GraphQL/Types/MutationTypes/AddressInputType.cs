430ec917a91138e6784bd8c18cf236c24ff87938
Mcebisi Mkhohliwe <mcebisi.mkhohliwe@sourcing-sa.co.za>
2020-08-02 19:16:31 +0200

    Added address input type

---
+++ Registration.API/GraphQL/Types/MutationTypes/AddressInputType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace Registration.API.GraphQL_Types.MutationTypes
+{
+    public class AddressInputType : InputObjectGraphType
+    {
+        public AddressInputType()
+        {
+            Field<StringGraphType>("unit");
+            Field<NonNullGraphType<StringGraphType>>("street");
+            Field<NonNullGraphType<StringGraphType>>("town");
+            Field<NonNullGraphType<StringGraphType>>("province");
+        }
+    }
+}
