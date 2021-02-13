b222f43c4be2a43a399d95d8bfa3da6167253378
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2021-02-13 22:04:32 +0200

    Added update and exists methods

--- Registration.Repositories/Contracts/IAddressRepository.cs
+++ Registration.Repositories/Contracts/IAddressRepository.cs
@@ -1,16 +1,18 @@
 ﻿using Registration.Entities.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Registration.Repository.Contracts
 {
     public interface IAddressRepository
     {
-        Task<int> Add(Address address);
+        Task<Address> Add(Address address);
+        Task<Address> Update(Address oldAddress, Address newAddress);
         Task<Address> GetById(int id);
         Task<IEnumerable<Address>> GetAll();
         Task<IEnumerable<Address>> GetByStreet(string name);
+        bool Exists(Expression<Func<Address, bool>> expression);
     }
 }
--- Registration.Repositories/Contracts/ICourseRepository.cs
+++ Registration.Repositories/Contracts/ICourseRepository.cs
@@ -1,13 +1,17 @@
 ﻿using Registration.Entities.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Registration.Repository.Contracts
 {
     public interface ICourseRepository
     {
-        Task<int> Add(Course course);
+        Task<Course> Add(Course course);
+        Task<Course> Update(Course oldCourse, Course newCourse);
         Task<IEnumerable<Course>> GetAll();
         Task<Course> GetById(int id);
+        bool Exists(Expression<Func<Course, bool>> expression);
     }
 }
